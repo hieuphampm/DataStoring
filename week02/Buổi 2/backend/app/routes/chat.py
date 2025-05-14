9c9740a959455d308b3c00bab4cab1773e791e67
Phuoc Hieu <hieuphamminh976@gmail.com>
2025-05-14 14:03:52 +0700

    update api

--- week02/Buổi 2/backend/app/routes/chat.py
+++ week02/Buổi 2/backend/app/routes/chat.py
@@ -13,5 +13,14 @@ class Message(BaseModel):
 async def send_message(msg: Message):
     try:
         r.rpush("chat:messages", f"{msg.user}: {msg.message}")
-    finally:
-        r.close()   
+        return {"status": "success", "message": "Message stored"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@chat.get("/messages")
+async def get_messages():
+    try:
+        message = r.lrange("chat:messages", 0, -1)
+        return {"messages": message}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
