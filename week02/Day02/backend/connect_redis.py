a0f681ea9a4d50362943a5967256284bb99e7576
Phuoc Hieu <hieuphamminh976@gmail.com>
2025-05-19 10:56:48 +0700

    up

--- week02/Day02/backend/connect_redis.py
+++ week02/Day02/backend/connect_redis.py
@@ -1,8 +1,12 @@
+import os
 import redis
 
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
 r = redis.Redis(
-    host='10.148.0.2',
-    port=6379,
+    host=redis_host,
+    port=redis_port,
     decode_responses=True,
-    socket_timeout=5  
+    socket_timeout=5
 )
