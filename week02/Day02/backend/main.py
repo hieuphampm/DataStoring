95c12dba4d0ed70a934fde0d23d223a25adfb74a
Phuoc Hieu <hieuphamminh976@gmail.com>
2025-05-20 21:34:37 +0700

    update

--- week02/Day02/backend/main.py
+++ week02/Day02/backend/main.py
@@ -6,13 +6,13 @@ app = FastAPI()
 
 allow_origins=["*"] 
 
-# Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow frontend origin
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],  # Allow necessary methods
-    allow_headers=["Content-Type"],  # Allow necessary headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 app.include_router(chat)
